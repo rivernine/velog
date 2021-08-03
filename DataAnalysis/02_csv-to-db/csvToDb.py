c475aa33ac71e62326613c1ffd163c802d7a7651
rivernine <rivernine@github.com>
2021-08-03 17:50:40 +0900

    update

--- DataAnalysis/02_csv-to-db/csvToDb.py
+++ DataAnalysis/02_csv-to-db/csvToDb.py
@@ -6,14 +6,15 @@ conn = pymysql.connect(host='192.168.56.100', port=3306, user='root', password='
 curs = conn.cursor()
 conn.commit()
 
-f = open('C:/Users/LGCNS/Downloads/2019-Nov.csv','r')
+f = open('C:/Users/LGCNS/Downloads/2019-Oct.csv','r')
 csvReader = csv.reader(f)
 # header skip
 next(csvReader)
 
-
-for cnt, row in enumerate(csvReader):
+count = 0
+for _, row in enumerate(csvReader):
   event_time = (row[0])
+  event_type = (row[1])
   product_id = (row[2])
   category_id = (row[3])
   category_code = (row[4])
@@ -21,10 +22,13 @@ for cnt, row in enumerate(csvReader):
   price = (row[6])
   user_id = (row[7])
 
+  if event_type != "purchase":
+    continue
   sql = """insert into orders (time, product_id, category_id, category_code, brand, price, user_id) values (%s, %s, %s, %s, %s, %s, %s)"""
   curs.execute(sql, (event_time, product_id, category_id, category_code, brand, price, user_id))
-  if cnt % 50000 == 0:
-    print(cnt)
+  count += 1
+  if count % 10000 == 0:
+    print(count)
     conn.commit()
 
 #db의 변화 저장
